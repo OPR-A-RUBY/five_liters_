023bf23d18f201cf932772521506cb70fdc35fe7
OPR-A-RUBin <opr.a.ruby@gmail.com>
2023-02-18 14:28:06 +0400

    Create hash of Alphavit liters

--- app.rb
+++ app.rb
@@ -2,5 +2,25 @@
 # 5 букв
 # для Тинькофф
 
-def begin_screen
+def begin_screen # Очистка экрана (для Linux)
+end
+
+def alphavit
+end
+#          0        1         2         3
+#          12345678901234567890123456789012 
+alphavit = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯ'
+
+arr_b = alphavit.split("")
+arr_0 = [0,0]
+
+puts alphavit
+puts arr_b
+puts arr_0
+
+arr_b.each do |item|
+
+ hh_b = {item => arr_0}
+ puts hh_b
+
 end
