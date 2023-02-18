2bd961ab880b9167b44672f122fd6dc6661acfee
OPR-A-RUBin <opr.a.ruby@gmail.com>
2023-02-18 14:53:46 +0400

    _added discription

--- app.rb
+++ app.rb
@@ -7,22 +7,24 @@ end
 
 def alphavit
 end
-#          0        1         2         3
-#          12345678901234567890123456789012 
+#            0        1         2         3
+#            12345678901234567890123456789012 
  alphavit = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯ'
 
- arr_b = alphavit.split("")
- arr_0 = [0,0]
+ arr_b = alphavit.split("") # ['А', 'Б', 'В', 'Г', 'Д', 'Е', ... 'Я']
+ arr_0 = [0,0]              # Два флага для описания состояния буквы
+                            # 0-ой - наличие буквы в слове
+			    # 1-ый - стоит на нужном месте (здесь ошибка)
 
- puts alphavit
- puts arr_b
- puts arr_0
+ puts alphavit              # DEBUG
+ puts arr_b                 # DEBUG
+ puts arr_0                 # DEBUG
 
- hh_b = {}
+ hh_b = {}                  # Хэш для описания состояний букв
  arr_b.each do |item|
 
    hh_b [item] = arr_0
 
  end
 
- puts hh_b
+ puts hh_b                  # DEBUG
