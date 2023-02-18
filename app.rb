8cbd336bc3bc80059c704bf2a8a487cf35b6f286
OPR-A-RUBin <opr.a.ruby@gmail.com>
2023-02-18 14:31:55 +0400

    bestly

--- app.rb
+++ app.rb
@@ -9,18 +9,20 @@ def alphavit
 end
 #          0        1         2         3
 #          12345678901234567890123456789012 
-alphavit = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯ'
+ alphavit = 'АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯ'
 
-arr_b = alphavit.split("")
-arr_0 = [0,0]
+ arr_b = alphavit.split("")
+ arr_0 = [0,0]
 
-puts alphavit
-puts arr_b
-puts arr_0
+ puts alphavit
+ puts arr_b
+ puts arr_0
 
-arr_b.each do |item|
+ hh_b = {}
+ arr_b.each do |item|
 
- hh_b = {item => arr_0}
+   hh_b [item] = arr_0
+
+ end
+
  puts hh_b
-
-end
